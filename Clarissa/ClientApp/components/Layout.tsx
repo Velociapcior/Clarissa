b942d7fe4787cc12ec241a14279e1d5a2dcdfab1
velociapcior <piotr.palej91@gmail.com>
2018-02-23 17:29:47 +0100

    nowy layut

--- Clarissa/ClientApp/components/Layout.tsx
+++ Clarissa/ClientApp/components/Layout.tsx
@@ -11,52 +11,54 @@ export class Layout extends React.Component<LayoutProps, {}> {
         return (
             <div>
                 <nav className="navbar" role="navigation">
-                    <div className="navbar-brand">
-                        <i className="fa fa-btc"></i>
-                    </div>
-                    <div className="navbar-menu">
-                        <div className="navbar-start">
-
-                            <a className="navbar-item" href="#">
-                                <span className="icon has-text-info">
-                                    <i className="fa fa-facebook-square"></i>
-                                </span>
-                            </a>
-
-                            <a className="navbar-item" href="#">
-                                <span className="icon has-text-info">
-                                    <i className="fa fa-twitter-square"></i>
-                                </span>
-                            </a>
-                            <a className="navbar-item" href="#">
-                                <span className="icon has-text-danger">
-                                    <i className="fa fa-youtube-square"></i>
-                                </span>
-                            </a>
+                    <div className="container">
+                        <div className="navbar-brand">
+                            <i className="fa fa-btc"></i>
                         </div>
-                        <div className="navbar-end">
-                            <div className="navbar-item has-dropdown is-hoverable">
-                                <a className="navbar-link">
-                                    Docs
+                        <div className="navbar-menu">
+                            <div className="navbar-start">
+
+                                <a className="navbar-item" href="#">
+                                    <span className="icon has-text-info">
+                                        <i className="fa fa-facebook-square"></i>
+                                    </span>
                                 </a>
-                                <div className="navbar-dropdown">
-                                    <NavLink to={"/Alert"} className="navbar-item" exact activeClassName="is-active">
-                                        <span>Alert</span>
+
+                                <a className="navbar-item" href="#">
+                                    <span className="icon has-text-info">
+                                        <i className="fa fa-twitter-square"></i>
+                                    </span>
+                                </a>
+                                <a className="navbar-item" href="#">
+                                    <span className="icon has-text-danger">
+                                        <i className="fa fa-youtube-square"></i>
+                                    </span>
+                                </a>
+                            </div>
+                            <div className="navbar-end">
+                                <div className="navbar-item has-dropdown is-hoverable">
+                                    <a className="navbar-link">
+                                        Docs
+                                </a>
+                                    <div className="navbar-dropdown">
+                                        <NavLink to={"/Alert"} className="navbar-item" exact activeClassName="is-active">
+                                            <span>Alert</span>
+                                        </NavLink>
+                                        <NavLink to={"/User"} className="navbar-item" exact activeClassName="is-active">
+                                            User
                                     </NavLink>
-                                    <NavLink to={"/User"} className="navbar-item" exact activeClassName="is-active">
-                                        User
-                                    </NavLink>
-                                    <a className="navbar-item">
-                                        Components
+                                        <a className="navbar-item">
+                                            Components
                                     </a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </nav>
                 <div className="columns">
-                    <div className="column is-one-fifth">
-                            <NavMenu /> 
+                    <div className="column is-1">
+                        <NavMenu />
                     </div>
                     <div className="column">
                         <div className="container">
